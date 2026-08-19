a9771f1b2b27a191efb0a3eb69255a992b71a80f
theabhishekbhujang <theabhishekbhujang@users.noreply.github.com>
2026-08-19 23:41:34 +0530

    fix(rhythm): unlock all Pro features, scales, Manjeera, variations and disable purchase prompts

--- patches/src/main/kotlin/app/template/patches/rhythm/RhythmPatches.kt
+++ patches/src/main/kotlin/app/template/patches/rhythm/RhythmPatches.kt
@@ -1,14 +1,13 @@
 package app.template.patches.rhythm
 
 import app.morphe.patcher.Fingerprint
-import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.extensions.InstructionExtensions.replaceInstructions
 import app.morphe.patcher.patch.bytecodePatch
 import app.template.patches.shared.Constants.COMPATIBILITY_RHYTHM
 
 val unlockProPatch = bytecodePatch(
     name = "Unlock Pro",
-    description = "Unlocks all Pro features, taals, variations, scales, and removes ads.",
+    description = "Unlocks all Pro features, taals, variations, scales, Manjeera, Tanpuras, removes purchase banners and ads.",
     default = true
 ) {
     compatibleWith(COMPATIBILITY_RHYTHM)
@@ -40,7 +39,52 @@ val unlockProPatch = bytecodePatch(
             """
         )
 
-        // 3. Unlock PickerView feature restrictions
+        // 3. Force m4.f constructor to set isPro = true (b = false) so all taals & variations load
+        Fingerprint(
+            definingClass = "Lm4/f;",
+            name = "<init>",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+                new-instance v0, Ljava/util/ArrayList;
+                invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
+                iput-object v0, p0, Lm4/f;->e:Ljava/util/ArrayList;
+                new-instance v0, Ljava/util/ArrayList;
+                invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
+                iput-object v0, p0, Lm4/f;->f:Ljava/util/ArrayList;
+                new-instance v0, Ljava/util/ArrayList;
+                invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
+                iput-object v0, p0, Lm4/f;->g:Ljava/util/ArrayList;
+                new-instance v0, Ljava/util/ArrayList;
+                invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
+                iput-object v0, p0, Lm4/f;->h:Ljava/util/ArrayList;
+                iput-object p1, p0, Lm4/f;->a:Landroid/content/Context;
+                const/4 v0, 0x0
+                iput-boolean v0, p0, Lm4/f;->b:Z
+                iput-object p3, p0, Lm4/f;->c:Lk4/z;
+                iput-object p4, p0, Lm4/f;->d:Lm4/f$a;
+                return-void
+            """
+        )
+
+        // 4. Force PickerView.setIsEnabled(Z) to always set s = true (Unlocks Scale Picker, Tanpura Voice Picker)
+        Fingerprint(
+            definingClass = "Lcom/psslabs/rhythm/helper/PickerView;",
+            name = "setIsEnabled",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                const/4 p1, 0x1
+                iput-boolean p1, p0, Lcom/psslabs/rhythm/helper/PickerView;->s:Z
+                invoke-direct {p0}, Lcom/psslabs/rhythm/helper/PickerView;->m()V
+                return-void
+            """
+        )
+
+        // 5. Unlock PickerView restrictions
         Fingerprint(
             definingClass = "Lcom/psslabs/rhythm/helper/PickerView;",
             name = "h",
@@ -53,19 +97,56 @@ val unlockProPatch = bytecodePatch(
             """
         )
 
-        // 4. Force m4.f constructor parameter `isPro` to false so all taals are loaded into e list
+        // 6. Force Ln4/e.n() to return true (Unlocks Manjeera and all instruments)
         Fingerprint(
-            definingClass = "Lm4/f;",
-            name = "<init>",
-            returnType = "V"
-        ).method.addInstructions(
-            14,
+            definingClass = "Ln4/e;",
+            name = "n",
+            returnType = "Z"
+        ).method.replaceInstructions(
+            0,
             """
-                const/4 p2, 0x0
+                const/4 v0, 0x1
+                return v0
             """
         )
 
-        // 5. Bypass Store Check (m4.f.n())
+        // 7. Silence "Please purchase the premium version" purchase prompts (Li4/b.T1())
+        Fingerprint(
+            definingClass = "Li4/b;",
+            name = "T1",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+
+        // 8. Disable StoreActivity launcher (StoreActivity.Z1())
+        Fingerprint(
+            definingClass = "Lcom/psslabs/rhythm/StoreActivity;",
+            name = "Z1",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+
+        // 9. Disable Get Premium button click (ListingActivity$b.onClick())
+        Fingerprint(
+            definingClass = "Lcom/psslabs/rhythm/ListingActivity${'$'}b;",
+            name = "onClick",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+
+        // 10. Bypass Store Check (m4.f.n())
         Fingerprint(
             definingClass = "Lm4/f;",
             name = "n",
@@ -78,7 +159,7 @@ val unlockProPatch = bytecodePatch(
             """
         )
 
-        // 6. Disable Banner Ads (k4.d.f())
+        // 11. Disable Banner Ads (k4.d.f())
         Fingerprint(
             definingClass = "Lk4/d;",
             name = "f",
@@ -90,10 +171,58 @@ val unlockProPatch = bytecodePatch(
             """
         )
 
-        // 7. Disable Interstitial Ads (k4.d.g())
+        // 12. Disable Interstitial Ads (k4.d.g())
         Fingerprint(
             definingClass = "Lk4/d;",
             name = "g",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+
+        // 13. Disable Ad Thread Launch (k4.d.k())
+        Fingerprint(
+            definingClass = "Lk4/d;",
+            name = "k",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+
+        // 14. Disable Interstitial Load (k4.d.m())
+        Fingerprint(
+            definingClass = "Lk4/d;",
+            name = "m",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+
+        // 15. Disable Ad Activity Load (k4.d.p())
+        Fingerprint(
+            definingClass = "Lk4/d;",
+            name = "p",
+            returnType = "V"
+        ).method.replaceInstructions(
+            0,
+            """
+                return-void
+            """
+        )
+
+        // 16. Disable Banner Ad Load (k4.d.q())
+        Fingerprint(
+            definingClass = "Lk4/d;",
+            name = "q",
             returnType = "V"
         ).method.replaceInstructions(
             0,
