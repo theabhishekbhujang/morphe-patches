82a2445f072ed136701c0f5148d47b8b1eff0030
theabhishekbhujang <theabhishekbhujang@users.noreply.github.com>
2026-08-19 21:33:15 +0530

    fix(rhythm): match 18 instruction count in replacement smali for m4.f constructor

--- patches/src/main/kotlin/app/template/patches/rhythm/RhythmPatches.kt
+++ patches/src/main/kotlin/app/template/patches/rhythm/RhythmPatches.kt
@@ -59,7 +59,6 @@ val unlockProPatch = bytecodePatch(
             returnType = "V"
         ).method.replaceInstructions(
             0,
-            18,
             """
                 invoke-direct {p0}, Ljava/lang/Object;-><init>()V
                 new-instance v0, Ljava/util/ArrayList;
@@ -75,10 +74,9 @@ val unlockProPatch = bytecodePatch(
                 invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
                 iput-object v0, p0, Lm4/f;->h:Ljava/util/ArrayList;
                 iput-object p1, p0, Lm4/f;->a:Landroid/content/Context;
-                const/4 v0, 0x0
-                iput-boolean v0, p0, Lm4/f;->b:Z
+                const/4 p2, 0x0
+                iput-boolean p2, p0, Lm4/f;->b:Z
                 iput-object p3, p0, Lm4/f;->c:Lk4/z;
-                iput-object p4, p0, Lm4/f;->d:Lm4/f${'$'}a;
                 return-void
             """
         )
